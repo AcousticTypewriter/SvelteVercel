7147f0e0bca31d42f9c151b9d964694e304feb53
AcousticTypewriter <111464215+AcousticTypewriter@users.noreply.github.com>
2023-04-28 15:50:12 +0000

    Fonts again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <slot></slot>
 
 <style>
+	@font-face {
+	  font-family: Lato;
+	  src: url('/fonts/UbuntnuMono_Bold.tff') format('tff');
+	}
+	
 	*{
 		box-sizing: inherit;
 		font-family: "Ubuntu Mono";
 	}
 	
 </style>
-<script>
-	import "@fontsource/ubuntu-mono"
-</script>
